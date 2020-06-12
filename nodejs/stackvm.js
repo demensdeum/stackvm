3ce2ad124aff40a13bc8844f11a05b81b92d24c4
Ilya Prokhorov <i.prokhorov@key-g.com>
2020-06-12 23:57:42 +0300

    Numbers parsing implemented

--- nodejs/stackvm.js
+++ nodejs/stackvm.js
@@ -4,6 +4,8 @@ const OPCODE_SUM = '+';
 const OPCODE_SUB = '-';
 const OPCODE_MUL = '*';
 const OPCODE_DIV = '/';
+const OPCODE_PARSE_NUMBER_START = "Н";
+const OPCODE_PARSE_NUMBER_END = "н";
 const OPCODE_CALCULATOR_MODE = "К";
 const STACKVM_ARITHMETIC_OPERATIONS = [OPCODE_SUM, OPCODE_SUB, OPCODE_DIV, OPCODE_MUL];
 
@@ -64,6 +66,13 @@ class StackVM {
         }
         process.stdout.write("\n")
       }
+      else if (character == OPCODE_PARSE_NUMBER_START) {
+        var numberString = "";
+        for (let stackFirst = this.stack.pop(); stackFirst.value != OPCODE_PARSE_NUMBER_END; stackFirst = this.stack.pop()) {
+          numberString += stackFirst.value;
+        }
+        this.stack.push(numberString);
+      }
       else if (character == OPCODE_SUM) {
         let rhs = parseInt(this.stack.pop().value);
         let lhs = parseInt(this.stack.pop().value);
@@ -97,34 +106,58 @@ class StackVM {
         promptInterface.question('Calculator mode: ', (inputString) => {
           promptInterface.close();
           var outputProgram = "";
+
+          var isNumberParseMode = false;
+          var parsedNumber = "";
+
           for (let calculatorModeCharacter of inputString) {
             let parsedInt = parseInt(calculatorModeCharacter);
             if (isNaN(parsedInt) == false) {
-              outputProgram += String(parsedInt);
-            }
-            else if (calculatorModeCharacter == '(') {
-              this.stack.push(calculatorModeCharacter);
-            }
-            else if (calculatorModeCharacter == ')') {
-              for (let stackFirst = this.stack.pop(); stackFirst.value != '('; stackFirst = this.stack.pop()) {
-                outputProgram += String(stackFirst.value);
+              if (isNumberParseMode == false) {
+                outputProgram += OPCODE_PARSE_NUMBER_END;
               }
+              isNumberParseMode = true;
+              parsedNumber += String(parsedInt);
             }
-            else if (STACKVM_ARITHMETIC_OPERATIONS.includes(calculatorModeCharacter)) {
-                let first = this.stack.pop();
-                if (first != null) {
-                    let operation = first.value
-                    let lhsPriority = STACKVM_OPERATION_PRIORITIES[calculatorModeCharacter];
-                    let rhsPriority = STACKVM_OPERATION_PRIORITIES[operation];
-                    if (STACKVM_ARITHMETIC_OPERATIONS.includes(operation) && lhsPriority >= rhsPriority) {
-                      outputProgram += first.value;
-                    }
-                    else {
-                      this.stack.push(operation);
-                    }
-                }
+            else {
+              if (isNumberParseMode) {
+                isNumberParseMode = false;
+                outputProgram += reverseString(parsedNumber);
+                outputProgram += OPCODE_PARSE_NUMBER_START
+                parsedNumber = "";
+              }
+              if (calculatorModeCharacter == '(') {
                 this.stack.push(calculatorModeCharacter);
+              }
+              else if (calculatorModeCharacter == ')') {
+                for (let stackFirst = this.stack.pop(); stackFirst.value != '('; stackFirst = this.stack.pop()) {
+                  outputProgram += String(stackFirst.value);
+                }
+              }
+              else if (STACKVM_ARITHMETIC_OPERATIONS.includes(calculatorModeCharacter)) {
+                  let first = this.stack.pop();
+                  if (first != null) {
+                      let operation = first.value
+                      let lhsPriority = STACKVM_OPERATION_PRIORITIES[calculatorModeCharacter];
+                      let rhsPriority = STACKVM_OPERATION_PRIORITIES[operation];
+                      if (STACKVM_ARITHMETIC_OPERATIONS.includes(operation) && lhsPriority >= rhsPriority) {
+                        outputProgram += first.value;
+                      }
+                      else {
+                        this.stack.push(operation);
+                      }
+                  }
+                  this.stack.push(calculatorModeCharacter);
+              }
+
+
             }
+          }
+
+          if (isNumberParseMode) {
+            outputProgram += reverseString(parsedNumber);
+            outputProgram += OPCODE_PARSE_NUMBER_START;
+            isNumberParseMode = false;
           }
 
           for (let stackFirst = this.stack.pop(); stackFirst != null; stackFirst = this.stack.pop()) {
